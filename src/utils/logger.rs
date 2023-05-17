21e9dd5442222b365ef66c2237261677eb2efa30
jaime-pue <jaime.alvarez@pue.es>
2023-05-17 11:53:50 +0200

    Configure logger

--- src/utils/logger.rs
+++ src/utils/logger.rs
@@ -1,26 +1,26 @@
-use std::env;
-
-
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
 use crate::configuration::settings::set_logger_level;
 
 pub fn setup_logger() {
+    let level = set_level();
+    let subscriber = FmtSubscriber::builder()
+        .with_target(false)
+        .with_max_level(level)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+}
+
+fn set_level() -> Level {
     let binding = set_logger_level();
     let input = binding.as_str();
-    let level = match  input {
+    match input {
         "debug" => Level::DEBUG,
         "info" => Level::INFO,
         "error" => Level::ERROR,
         "warn" => Level::WARN,
         "trace" => Level::TRACE,
-        _ => Level::DEBUG
-    };
-    let env = format!("webapp_test={level},tower_http={level}");
-    env::set_var("RUST_LOG", env);
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(level)
-        .finish();
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+        _ => Level::DEBUG,
+    }
 }
