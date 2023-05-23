b85a6b666a5be8a5daa97f206b820d844719c1ed
jaime-pue <jaime.alvarez@pue.es>
2023-05-23 16:19:44 +0200

    Add session layer

--- src/controller/user_controller.rs
+++ src/controller/user_controller.rs
@@ -1,17 +1,25 @@
 use crate::{
     data::user_dto::{FindUserDto, LoginDto, NewUserDto},
-    service::user_service::{
-        create_user_service, find_user_service, get_all_users_service, login_service,
+    error::error::ApiError,
+    model::session_model::CurrentUser,
+    service::{
+        session_service::login_session,
+        user_service::{
+            create_user_service, find_user_service, get_all_users_service, login_service,
+        },
     },
 };
 use axum::{
-    response::IntoResponse,
+    response::{IntoResponse},
     routing::{get, post},
     Json, Router,
 };
+use axum_session::SessionRedisPool;
+use axum_session_auth::AuthSession;
 
 pub(crate) fn route_user() -> Router {
     let routes = Router::new()
+        .route("/", get(test_welcome))
         .route("/register", post(register_new_user))
         .route("/all", get(get_all_users))
         .route("/user", post(find_user))
@@ -19,17 +27,28 @@ pub(crate) fn route_user() -> Router {
     Router::new().nest("/auth", routes)
 }
 
-async fn register_new_user(Json(new_user): Json<NewUserDto>) -> impl IntoResponse {
-    match create_user_service(new_user).await {
-        Ok(user) => Ok(Json(user)),
-        Err(error) => Err(error),
-    }
+async fn register_new_user(
+    auth: AuthSession<CurrentUser, i64, SessionRedisPool, redis::Client>,
+    Json(new_user): Json<NewUserDto>,
+) -> impl IntoResponse {
+        match create_user_service(new_user).await {
+            Ok(user) => {
+                login_session(auth, user.1).await;
+                Ok(Json(user.0))},
+            Err(error) => Err(error),
+        }
 }
 
-async fn get_all_users() -> impl IntoResponse {
-    match get_all_users_service().await {
-        Ok(list) => Ok(Json(list)),
-        Err(error) => Err(error),
+async fn get_all_users(
+    auth: AuthSession<CurrentUser, i64, SessionRedisPool, redis::Client>,
+) -> impl IntoResponse {
+    if auth.current_user.unwrap().is_admin() {
+        match get_all_users_service().await {
+            Ok(list) => Ok(Json(list)),
+            Err(error) => Err(error),
+        }
+    } else {
+        Err(ApiError::Forbidden)
     }
 }
 
@@ -40,9 +59,25 @@ async fn find_user(Json(data): Json<FindUserDto>) -> impl IntoResponse {
     }
 }
 
-async fn login_user(Json(login): Json<LoginDto>) -> impl IntoResponse {
-    match login_service(login).await {
-        Ok(user) => Ok(Json(user)),
-        Err(error) => Err(error),
-    }
+async fn login_user(
+    auth: AuthSession<CurrentUser, i64, SessionRedisPool, redis::Client>,
+    Json(login): Json<LoginDto>,
+) -> impl IntoResponse {
+        match login_service(login).await {
+            Ok(user) => {
+                login_session(auth, user.1).await;
+
+                Ok(Json(user.0))
+            }
+            Err(error) => Err(error),
+        }
+}
+
+async fn test_welcome(
+    auth: AuthSession<CurrentUser, i64, SessionRedisPool, redis::Client>,
+) -> String {
+    format!(
+        "Hello, {}. This is a debug endpoint. Nothing to be found around here.\nCredentials: {:?}",
+        auth.current_user.clone().unwrap().username(), auth.current_user
+    )
 }
