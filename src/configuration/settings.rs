16b14d0a325debe88c248cdbc0a04801ddaa2576
jaime-pue <jaime.alvarez@pue.es>
2023-05-23 16:19:11 +0200

    Add more env variables

--- src/configuration/settings.rs
+++ src/configuration/settings.rs
@@ -2,41 +2,36 @@ use std::env;
 
 use dotenvy::dotenv;
 
-#[derive(Debug, Clone)]
-pub struct Server {
-    pub address: String,
-    pub port: String,
-}
-
-#[derive(Debug, Clone)]
-pub struct Logger {
-    pub level: String,
-}
-
-#[derive(Debug, Clone)]
-pub struct Database {
-    pub uri: String,
-}
-
-pub struct Settings {
-    pub database_url: Database,
-    pub logger_level: Logger,
-    pub server: Server,
-}
-
-pub fn set_database_url() -> String {
+pub fn database_url() -> String {
     let uri = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     uri
 }
 
-pub fn set_logger_level() -> String {
+pub fn logger_level() -> String {
     env::var("LOGGER_LEVEL").expect("LOGGER_LEVEL must be set")
 }
 
-pub fn set_server() -> String {
+pub fn server() -> String {
     let address = env::var("ADDRESS").expect("ADDRESS must be set");
     let port = env::var("PORT").expect("PORT must be set");
     format!("{address}:{port}")
+}
+
+pub fn redis_server() -> String {
+    let address = env::var("REDIS_ADDRESS").expect("REDIS_ADDRESS must be set");
+    let port = env::var("REDIS_PORT").expect("REDIS_PORT must be set");
+    format!("redis://{address}:{port}/") 
+}
+
+pub fn branch() -> String {
+    env::var("BRANCH").expect("BRANCH must be set")
+}
+
+pub fn session_default_duration() -> usize {
+    match env::var("SESSION_DURATION").expect("SESSION_DURATION must be set").parse::<usize>() {
+        Ok(duration) => duration,
+        Err(_) => 3600,
+    }
 }
 
 pub fn check_env_file() {
