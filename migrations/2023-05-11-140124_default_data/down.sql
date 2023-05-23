708bd0e867515bae322ab743d3f15cd997c043a7
jaime-pue <jaime.alvarez@pue.es>
2023-05-23 16:17:28 +0200

    Update default data

--- migrations/2023-05-11-140124_default_data/down.sql
+++ migrations/2023-05-11-140124_default_data/down.sql
@@ -4,18 +4,18 @@ DELETE from babies;
 DELETE from users_roles;
 DELETE from users_babies;
 
-insert into
-    users
-values
-    (0, "admin", "$2b$12$3X1BP9hIp.NpRbqSi5EZ3e.oH0qs53M0Tj7IGYnohpLbWgJvcmebK", "Test", "User", "admin@a.a");
+-- insert into
+--     users
+-- values
+--     (2, "admin", "$2b$12$3X1BP9hIp.NpRbqSi5EZ3e.oH0qs53M0Tj7IGYnohpLbWgJvcmebK", "Test", "User", "admin@a.a");
 
 -- associate admin user and roles
-insert into
-    users_roles
-VALUES
-    (0, 0, 0);
+-- insert into
+--     users_roles
+-- VALUES
+--     (0, 0, 0);
 
-insert into
-    users_roles
-VALUES
-    (1, 1, 0);
+-- insert into
+--     users_roles
+-- VALUES
+--     (1, 1, 0);
