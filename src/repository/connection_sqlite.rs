b0923290ed45712ed135727b3b87d13a8110a46b
jaime-pue <jaime.alvarez@pue.es>
2023-05-24 10:28:04 +0200

    Add db checks.

--- src/repository/connection_sqlite.rs
+++ src/repository/connection_sqlite.rs
@@ -1,9 +1,19 @@
 use diesel::{Connection, SqliteConnection};
 
-use crate::configuration::settings::database_url;
+use crate::{configuration::settings::database_url, error::error::ApiError};
 
 pub fn establish_connection() -> SqliteConnection {
     let database_url = database_url();
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn check_sqlite_status() -> Result<String, ApiError> {
+    let database_url = database_url();
+    match SqliteConnection::establish(&database_url).is_ok() {
+        true => Ok("SQLite ready.".to_string()),
+        false => Err(ApiError::Generic500Error(
+            "Something went wrong with SQLite.".to_string(),
+        )),
+    }
 }
