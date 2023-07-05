ea6fdaf09943979c3ce1e1ac8f9284de094a3c28
jaime-alv <jaime.alv.fdz@gmail.es>
2023-07-05 16:54:15 +0200

    Add more checks

--- src/service/util_service.rs
+++ src/service/util_service.rs
@@ -1,9 +1,15 @@
 use std::collections::HashMap;
 
-use chrono::NaiveDateTime;
+use chrono::{NaiveDate, NaiveDateTime};
 use hyper::StatusCode;
 
-use crate::{error::error::ApiError, utils::{response::Response, datetime::to_date_time}};
+use crate::{
+    error::error::ApiError,
+    utils::{
+        datetime::{date_is_higher, to_date_time},
+        response::Response,
+    },
+};
 
 pub fn ok(msg: &str) -> Response {
     Response::new(StatusCode::OK, msg)
@@ -27,5 +33,21 @@ pub fn uncover_date(date: Option<String>) -> Result<Option<NaiveDateTime>, ApiEr
             Err(e) => Err(e.into()),
         },
         None => Ok(None),
+    }
+}
+
+pub fn check_days_out_of_bounds(days: u64) -> Result<(), ApiError> {
+    if days.ge(&0) && days.lt(&61) {
+        Ok(())
+    } else {
+        Err(ApiError::OutOfBounds(0, 60))
+    }
+}
+
+pub fn dates_are_in_order(from: NaiveDate, to: NaiveDate) -> Result<(), ApiError> {
+    if date_is_higher(from, to) {
+        Ok(())
+    } else {
+        Err(ApiError::DatesUnordered)
     }
 }
