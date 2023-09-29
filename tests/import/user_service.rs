1eabe836cc6f96df7afd1c1e91835cb93bdac42f
Jaime-alv <jaime.alv.fdz@gmail.com>
2023-09-29 09:24:19 +0200

    Move user tests from src

---
+++ tests/import/user_service.rs
@@ -0,0 +1,43 @@
+use nighty_night::{
+    data::{
+        common_structure::{SessionDto, UserDto},
+        user_dto::{NewUserDto, UpdateUserDto},
+    },
+    response::{error::ApiError, response::RecordResponse},
+    service::user_service::{get_user_by_id_service, patch_user_service, post_new_user_service, validate_new_user_information},
+};
+
+pub async fn test_create_user(
+    user: &NewUserDto,
+) -> Result<(RecordResponse<SessionDto>, i32), ApiError> {
+    let new_user = NewUserDto {
+        username: user.username.to_owned(),
+        password: user.password.to_owned(),
+        email: user.email.to_owned(),
+        name: user.name.to_owned(),
+        surname: user.surname.to_owned(),
+    };
+    post_new_user_service(new_user).await
+}
+
+pub async fn test_load_user_profile(user_id: i32) -> Result<RecordResponse<UserDto>, ApiError> {
+    get_user_by_id_service(user_id).await
+}
+
+pub async fn test_patch_user_profile(
+    user_id: i32,
+    update_profile: &UpdateUserDto,
+) -> Result<RecordResponse<UserDto>, ApiError> {
+    let update_fields = UpdateUserDto {
+        email: update_profile.email.to_owned(),
+        name: update_profile.name.to_owned(),
+        surname: update_profile.surname.to_owned(),
+    };
+
+    let response = patch_user_service(user_id, update_fields).await;
+    response
+}
+
+pub fn test_validate_user_fields(user: NewUserDto) -> Result<(), ApiError> {
+    validate_new_user_information(&user)
+}
