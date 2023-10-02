6cc8ecc53b44bb6ab2af4becd6fae7551204b90b
Jaime-alv <jaime.alv.fdz@gmail.com>
2023-10-02 15:56:23 +0200

    Split baby tests

--- tests/test_baby_integration.rs
+++ tests/test_baby_integration.rs
@@ -1,10 +1,12 @@
+use fake::{faker::name::en::FirstName, Fake};
 use hyper::StatusCode;
 use nighty_night::{
-    data::query_dto::Pagination,
+    data::{baby_dto::InputBabyDto, query_dto::Pagination},
     service::{
         baby_service::{
             delete_baby_service, get_babies_for_user_service, get_baby_by_id_service,
-            post_new_baby_service, post_share_baby_with_user_service, transfer_baby_service,
+            patch_baby_service, post_new_baby_service, post_share_baby_with_user_service,
+            transfer_baby_service,
         },
         user_service::delete_user_from_database,
     },
@@ -16,10 +18,12 @@ use crate::{
             assert_compare_fields, assert_error_response, assert_len, assert_ok_message,
             assert_ok_paginated, assert_ok_response,
         },
-        cte::DB_ERROR,
+        cte::{DB_ERROR, DELETE},
     },
-    helper::{create_babies, delete_babies},
-    mock::{entities::create_new_user, generate_new_baby},
+    mock::{
+        entities::{create_new_baby, create_new_user},
+        generate_date, generate_new_baby,
+    },
 };
 
 pub mod common;
@@ -69,7 +73,26 @@ async fn test_baby_flow() {
         "Unique id should match",
     );
 
-    let babies = create_babies(BABIES_TO_ADD, user_id).await;
+    let response_delete_baby = delete_baby_service(test_baby_id, user_id).await;
+    assert_ok_message(
+        &response_delete_baby,
+        "Should delete baby from database",
+        StatusCode::OK,
+    );
+
+    delete_user_from_database(user_id).expect(DB_ERROR);
+}
+
+#[tokio::test]
+async fn test_add_several_babies() {
+    let (user_id, _user_credentials) = create_new_user().await;
+
+    let mut babies: Vec<i32> = Vec::new();
+    for _i in 0..BABIES_TO_ADD {
+        let id = create_new_baby(user_id).await;
+        babies.push(id)
+    }
+
     let response_user_babies = get_babies_for_user_service(user_id, Pagination::default()).await;
     assert_ok_paginated(
         &response_user_babies,
@@ -81,9 +104,31 @@ async fn test_baby_flow() {
 
     assert_len(
         &new_babies_added,
-        BABIES_TO_ADD + 1,
+        BABIES_TO_ADD,
         "User should have <X> babies",
     );
+
+    for baby in babies {
+        delete_baby_service(baby, user_id).await.expect(DELETE);
+    }
+
+    delete_user_from_database(user_id).expect(DB_ERROR);
+}
+
+#[tokio::test]
+async fn test_transfer_ownership() {
+    let (user_id, _user_credentials) = create_new_user().await;
+    let baby = generate_new_baby();
+    let response_new_baby = post_new_baby_service(baby, user_id).await;
+    assert_ok_response(
+        &response_new_baby,
+        "Should create a new baby",
+        StatusCode::CREATED,
+    );
+
+    let baby = response_new_baby.unwrap();
+    let test_baby_id: i32 = baby.data.id;
+    let test_baby_uid = baby.data.attributes.unique_id;
 
     let (test_user_two, _credentials) = create_new_user().await;
 
@@ -106,6 +151,7 @@ async fn test_baby_flow() {
         .unwrap()
         .attributes
         .unique_id;
+
     assert_compare_fields(
         &test_baby_uid.to_string(),
         &record_baby.to_string(),
@@ -125,11 +171,7 @@ async fn test_baby_flow() {
         .expect(DB_ERROR)
         .data;
 
-    assert_len(
-        &updated_babies_count,
-        BABIES_TO_ADD + 1,
-        "Should maintain babies count",
-    );
+    assert_len(&updated_babies_count, 1, "Should maintain babies count");
 
     let response_delete_baby = delete_baby_service(test_baby_id, user_id).await;
     assert_ok_message(
@@ -142,11 +184,7 @@ async fn test_baby_flow() {
         .expect(DB_ERROR)
         .data;
 
-    assert_len(
-        &updated_babies_count,
-        BABIES_TO_ADD,
-        "Should loose one baby count",
-    );
+    assert_len(&updated_babies_count, 0, "Should loose one baby count");
 
     let response_baby_id = get_baby_by_id_service(test_baby_id).await;
     assert_ok_response(
@@ -170,28 +208,74 @@ async fn test_baby_flow() {
     );
 
     // Tear down examples.
-    delete_babies(babies, user_id).await;
     delete_user_from_database(user_id).expect(DB_ERROR);
     delete_user_from_database(test_user_two).expect(DB_ERROR);
 }
 
-mod helper {
-    use nighty_night::service::baby_service::delete_baby_service;
+#[tokio::test]
+async fn test_update_baby_info() {
+    let (user_id, _user_credentials) = create_new_user().await;
+    let baby = generate_new_baby();
+    let new_baby = post_new_baby_service(baby, user_id)
+        .await
+        .expect(DB_ERROR)
+        .data;
 
-    use crate::{common::cte::DELETE, mock::entities::create_new_baby};
+    let update_name = InputBabyDto {
+        name: Some(FirstName().fake()),
+        birthdate: None,
+    };
+    let update_name_field = &update_name.name.to_owned().unwrap();
 
-    pub(super) async fn create_babies(number: i32, user_id: i32) -> Vec<i32> {
-        let mut babies_id: Vec<i32> = Vec::new();
-        for _i in 0..number {
-            let id = create_new_baby(user_id).await;
-            babies_id.push(id)
-        }
-        babies_id
-    }
+    let response_patch_baby = patch_baby_service(new_baby.id, update_name).await;
+    assert_ok_response(
+        &response_patch_baby,
+        "Should update baby information",
+        StatusCode::OK,
+    );
 
-    pub(super) async fn delete_babies(babies: Vec<i32>, user_id: i32) {
-        for baby in babies {
-            delete_baby_service(baby, user_id).await.expect(DELETE);
-        }
-    }
+    let patched_baby = response_patch_baby.expect(DB_ERROR).data;
+
+    assert_compare_fields(
+        update_name_field,
+        &patched_baby.attributes.name,
+        "Name should be the same",
+    );
+
+    let bad_format_date = InputBabyDto {
+        name: None,
+        birthdate: Some("abcd".to_string()),
+    };
+
+    let bad_date = patch_baby_service(new_baby.id, bad_format_date)
+        .await
+        .expect(DB_ERROR)
+        .data;
+
+    assert_compare_fields(
+        &new_baby.attributes.birthdate,
+        &bad_date.attributes.birthdate,
+        "Should not change date",
+    );
+
+    let update_birthdate = InputBabyDto {
+        name: None,
+        birthdate: Some(generate_date()),
+    };
+    let update_birthdate_field = &update_birthdate.birthdate.to_owned().unwrap();
+
+    let patched_date = patch_baby_service(new_baby.id, update_birthdate)
+        .await
+        .expect(DB_ERROR)
+        .data;
+
+    assert_compare_fields(
+        update_birthdate_field,
+        &patched_date.attributes.birthdate,
+        "Date should be the same",
+    );
+
+    delete_baby_service(new_baby.id, user_id).await.expect(DB_ERROR);
+    delete_user_from_database(user_id).expect(DB_ERROR);
+
 }
